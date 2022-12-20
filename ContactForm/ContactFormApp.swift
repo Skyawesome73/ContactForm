a056baa00537fa82e34f10a83ab008f5be5d8044
Skyawesome73 <skyawesome73@gmail.com>
2022-12-20 10:27:25 -0600

    Initial Commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Nicholas Puig on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
